bcfbca666b1b7abeb14d883746225896947dc17a
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-01-22 20:46:47 +0000

    new colour scheme

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Home = () => {
-  return <section className="hero is-fullheight" id="home">
+  return <section className="hero is-black is-fullheight" id="home">
     <div className="hero-body">
       <div className="container has-text-centered" id="home-title">
         <p className="title">
           Jonny Garius
         </p>
-        <p className="subinfo" id="sub-style">Software Engineer | London</p>
+        <p className="subtitle">Software Engineer | London</p>
       </div>
     </div>
   </section>
