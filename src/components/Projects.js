2c3416c2508a8fdabb112d93224601188091f1e5
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-01-22 22:29:57 +0000

    styling changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,21 +11,93 @@ const Projects = () => {
   return <section className="section" id="projects">
     <div className="container has-text-centered">
       <p className="title">Projects</p>
-      <div>
-        <p className="subtitle">Placeholder</p>
+      <div className="project-child">
         <img src={placeholderImage} alt="placeholder screenshot"/>
+        <div className="project-child-description">
+          <p className="subtitle">Placeholder</p>
+          <div className="hozline"></div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem praesentium dignissimos earum nisi provident facilis. Eligendi eius aut dicta odit exercitationem architecto optio, a vero quaerat ipsam saepe! Consequatur, aliquid?</p>
+          <div className="project-child-skill">
+            <div className="github-container">
+              <div className="icon is-large">
+                <i className="skills-icon devicon-github-plain"></i>
+              </div>
+              <p className="is-size-7">github repo</p>
+            </div>
+            <div className="project-link-container">
+              <div className="icon is-large">
+                <i className="fas fa-external-link-alt fa-lg"></i>
+              </div>
+              <p className="is-size-7">go to project</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <p className="subtitle">artist_Flow</p>
-        <img src={artistFlowImage} alt="placeholder screenshot"/>
+      <div className="project-child">
+        <img src={artistFlowImage} alt="artist Flow screenshot"/>
+        <div className="project-child-description">
+          <p className="subtitle">artist_Flow</p>
+          <div className="hozline"></div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem praesentium dignissimos earum nisi provident facilis. Eligendi eius aut dicta odit exercitationem architecto optio, a vero quaerat ipsam saepe! Consequatur, aliquid?</p>
+          <div className="project-child-skill">
+            <div className="github-container">
+              <div className="icon is-large">
+                <i className="skills-icon devicon-github-plain"></i>
+              </div>
+              <p className="is-size-7">github repo</p>
+            </div>
+            <div className="project-link-container">
+              <div className="icon is-large">
+                <i className="fas fa-external-link-alt fa-lg"></i>
+              </div>
+              <p className="is-size-7">go to project</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>     
-        <p className="subtitle">NewsMe</p>
-        <img src={newsmeImage} alt="placeholder screenshot"/>
+      <div className="project-child">     
+        <img src={newsmeImage} alt="news me screenshot"/>
+        <div className="project-child-description">
+          <p className="subtitle">NewsMe</p>
+          <div className="hozline"></div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem praesentium dignissimos earum nisi provident facilis. Eligendi eius aut dicta odit exercitationem architecto optio, a vero quaerat ipsam saepe! Consequatur, aliquid?</p>
+          <div className="project-child-skill">
+            <div className="github-container">
+              <div className="icon is-large">
+                <i className="skills-icon devicon-github-plain"></i>
+              </div>
+              <p className="is-size-7">github repo</p>
+            </div>
+            <div className="project-link-container">
+              <div className="icon is-large">
+                <i className="fas fa-external-link-alt fa-lg"></i>
+              </div>
+              <p className="is-size-7">go to project</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <p className="subtitle">Snake!</p>
-        <img src={snakeImage} alt="placeholder screenshot"/>
+      <div className="project-child">
+        <img src={snakeImage} alt="snake screenshot"/>
+        <div className="project-child-description">
+          <p className="subtitle">Snake!</p>
+          <div className="hozline"></div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem praesentium dignissimos earum nisi provident facilis. Eligendi eius aut dicta odit exercitationem architecto optio, a vero quaerat ipsam saepe! Consequatur, aliquid?</p>
+          <div className="project-child-skill">
+            <div className="github-container">
+              <div className="icon is-large">
+                <i className="skills-icon devicon-github-plain"></i>
+              </div>
+              <p className="is-size-7">github repo</p>
+            </div>
+            <div className="project-link-container">
+              <div className="icon is-large">
+                <i className="fas fa-external-link-alt fa-lg"></i>
+              </div>
+              <p className="is-size-7">go to project</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
