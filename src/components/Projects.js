71d9a82315129e5a83b288ec320a548203aa298f
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-03-10 22:13:11 +0000

    chevron

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,16 +29,20 @@ const Projects = () => {
   return <section className="section" id="projects">
     <div className="container has-text-centered">
       <div className="title">
-        <span className="previous" onClick={handlePreviousClick}>previous</span>
         Projects
-        <span className="next" onClick={handleNextClick}>next</span>
       </div>
       <div className="slide-number-container">
+        <div className="icon" onClick={handlePreviousClick}>
+          <i className="fas fa-chevron-left"></i>
+        </div>
         {slideArray.map((element, i) => {
           return <div key={i}>
             {slideIndex === element ? <p className="slide-number" id={i}>{element}</p> : <p id={i}>{element}</p>}
           </div>
         })}
+        <div className="icon" onClick={handleNextClick}>
+          <i className="fas fa-chevron-right"></i>
+        </div>
       </div>
       <SingleProject slideIndex={slideIndex} />
     </div>
