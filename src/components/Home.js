418421af5c55deeba1b1032212a881b0a7712e0f
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-04-07 13:34:14 +0100

    animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,7 +36,7 @@ const Home = () => {
     })
   }
   return <>
-  <section className="hero is-fullheight">
+  <section className="hero is-fullheight" id="home">
     <div className="hero-body">
       <div className="container has-text-centered" id="home-title">
         <p className="title is-size-1">
