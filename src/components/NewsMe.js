eac24b18d57ca559d0f7af191178719e318dd34e
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-07-31 11:36:48 +0100

    spelling...

--- src/components/NewsMe.js
+++ src/components/NewsMe.js
@@ -7,7 +7,7 @@ const NewsMe = () => (
     <div className="project-child-description">
       <p className="title" id="newsme-title">NewsMe</p>
       <div className="hozline"></div>
-      <p>NewsMe is a frontend application built in a pair coding hackathon. It utilises the News API and openWether API to provide users with the most popular news stories via a keyword search. It also allows users to grab a snapshot of the weather or generate a 5 day forecast.</p>
+      <p>NewsMe is a frontend application built in a pair coding hackathon. It utilises the News API and openWeather API to provide users with the most popular news stories via a keyword search. It also allows users to grab a snapshot of the weather or generate a 5 day forecast.</p>
       <div className="project-child-skill">
         <div className="github-container">
           <a href="https://github.com/jgar123/project-2" target="_blank" rel="noopener noreferrer">
